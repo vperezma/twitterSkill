bee254fe7edf277f7f0c171b06bab3a9704cf406
Jeremy Robles <jeremy.robles56@gmail.com>
2016-06-27 08:56:16 -0700

    adding gulpfile to gitignore

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const zip = require('gulp-zip');
- 
-gulp.task('default', () => {
-	return gulp.src('./*')
-	.pipe(zip('skill_'+Date()+'.zip'))
-	.pipe(gulp.dest('dist'));
-});
