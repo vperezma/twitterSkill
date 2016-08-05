b81c197392ec9da5e1f3a4215727ab32ea92353c
vperezma <vperezma89@gmail.com>
2016-08-05 16:43:34 -0700

    initial push

--- index.js
+++ index.js
@@ -1,64 +1,29 @@
 var alexa = require('alexa-app');
 var app = new alexa.app();
+require('dotenv').config();
 
-/**
- * LaunchRequest.
- */
-app.launch(function(request,response) {
-	response.say('Hey there fancy pants!');
-	response.card("Hey there fancy pants!","This is an example card");
+console.log(process.env);
+
+var Twitter = require('twitter');
+var client = new Twitter({
+	consumer_key: process.env.consumer_key,
+	consumer_secret: process.env.consumer_secret,
+	access_token_key: process.env.access_token_key,
+	access_token_secret: process.env.access_token_secret
 });
 
-
-/**
- * IntentRequest.
- */
-app.intent('number',
-  {
-    'slots':{'number':'NUMBER'},
-    'utterances':[ 'say the number {1-100|number}' ]
-  },
-  function(request,response) {
-    var number = request.slot('number');
-    response.say('You asked for the number '+number);
-    response.shouldEndSession(true);
-    response.send();
-  }
-);
-
-
-/**
- * IntentRequest w/ asynchronous response.
- */
-app.intent('checkStatus', 
-	{
-    	'utterances':[ 
-    		'status check', 'what is the status', 'tell me the status'
-    	]
-  	},
-	function(request,response) {
-		setTimeout(function() {		// simulate an async request
-
-	        // This is async and will run after a brief delay
-	        response.say('Status is operational, mam!');
-	    
-	        // Must call send to end the original request
-	        response.send();
-		
-		}, 250);
-
-	    // Return false immediately so alexa-app doesn't send the response
-	    return false;
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	if (!error) {
+		console.log(tweets, 'here');
 	}
-);
-
-
-/**
- * Error handler for any thrown errors.
- */
-app.error = function(exception, request, response) {
-    response.say('Sorry, something bad happened');
-};
-
-// Connect to lambda
-exports.handler = app.lambda();
+	console.log(response);
+});
+client.post('statuses/update', {status: 'I Love Twitter for real'},  function(error, tweet, response) {
+	if(error) {
+	  console.log(error);
+	  throw error;
+  }
+	console.log(tweet, 'no, here');  // Tweet body.
+	console.log(response, 'no way ');  // Raw response object.
+});
